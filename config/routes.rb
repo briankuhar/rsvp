d7b6771325988b098edc54ddf138b5490df7cd77
Brian Kuhar <briankuhar@gmail.com>
2016-10-20 03:34:42 +0000

    Heroku setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  #ActiveAdmin.routes(self)
   devise_for :users, :controllers => { sessions: "users/sessions", registrations: "users/registrations" }
   resources :events do
     member do
